ac8e8e7e8eef14347a352eade797deb85b59997c
Rishi Sharma <rishi.sharma@epfl.ch>
2022-02-17 17:26:06 +0100

    Update file split script

--- split_into_files.py
+++ split_into_files.py
@@ -1,5 +1,10 @@
+import sys
 from decentralizepy.datasets.Femnist import Femnist
 
-f = Femnist()
 
-f.file_per_user("leaf/data/femnist/data/train", "leaf/data/femnist/per_user_data/train")
+if __name__ == "__main__":
+    f = Femnist(None, None, None)
+    assert len(sys.argv) == 3
+    frm = sys.argv[1]
+    to = sys.argv[2]
+    f.file_per_user(frm, to)
