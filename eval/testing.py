e8bfbe5b8ff7bbd8481fc247ebd7a4776991c7a4
Rishi Sharma <rishi.sharma@epfl.ch>
2022-03-08 21:32:56 +0100

    Models start at the same point; optional optimizer reset

--- eval/testing.py
+++ eval/testing.py
@@ -62,6 +62,6 @@ if __name__ == "__main__":
             args.log_dir,
             log_level[args.log_level],
             args.test_after,
-            args.reset_optimzer,
+            args.reset_optimizer,
         ],
     )
