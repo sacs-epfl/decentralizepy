7a2f9ac98ac7fea86476be6bb01978fb7cd3e198
Rishi Sharma <33762894+rishi-s8@users.noreply.github.com>
2023-03-10 17:11:40 +0100

    prune

--- tutorial/run_decentralized.sh
+++ tutorial/run_decentralized.sh
@@ -1,42 +1,25 @@
 #!/bin/bash
-my_home=/home/risharma/Gitlab/
 
-cp -r /mnt/nfs/risharma/Gitlab/decentralizepy/src/* $my_home/decentralizepy/src/
+decpy_path=../eval # Path to eval folder
+graph=regular_16.txt # Absolute path of the graph file
+config_file=config.ini
+cp $graph $config_file $decpy_path
 cd $decpy_path
 
-decpy_path=/mnt/nfs/risharma/Gitlab/decentralizepy/eval
-cd $decpy_path
-
-env_python=~/miniconda3/envs/decpy/bin/python3
-cluster_csv=/mnt/nfs/shared/cluster.csv
-graph=/mnt/nfs/risharma/Gitlab/decentralizepy/tutorial/regular_16.txt
-original_config=/mnt/nfs/risharma/Gitlab/decentralizepy/tutorial/config_celeba_sharing.ini
-config_file=~/tmp/config.ini
-# procs_per_machine="2 3 5 6"
-machines=1
+env_python=~/miniconda3/envs/decpy/bin/python3 # Path to python executable of the environment | conda recommended
+machines=1 # number of machines in the runtime
 iterations=80
 test_after=20
-eval_file=testingManual.py
-log_level=DEBUG
+eval_file=testing.py # decentralized driver code (run on each machine)
+log_level=INFO # DEBUG | INFO | WARN | CRITICAL
 
-m=`cat $(grep addresses_filepath $original_config | awk '{print $3}') | grep $(/sbin/ifconfig | grep 'inet 10.90' | awk '{print $2}') | cut -d'"' -f2`
+m=0 # machine id corresponding consistent with ip.json
 echo M is $m
 
-ips=`cat $(grep addresses_filepath $original_config | awk '{print $3}') | grep ':'`
-procs_per_machine=""
-while read line
-do
-ip=`echo $line | cut -d'"' -f4`
-echo ip is $ip
-procs=`cat $cluster_csv | grep $ip | cut -d',' -f2`
-echo procs is $procs
-procs_per_machine="$procs_per_machine $procs"
-done <<< $ips
+procs_per_machine=16 # 16 processes on 1 machine
 echo procs per machine is $procs_per_machine
 
-log_dir=$(date '+%Y-%m-%dT%H:%M')/machine$m
+log_dir=$(date '+%Y-%m-%dT%H:%M')/machine$m # in the eval folder
 mkdir -p $log_dir
 
-cp $original_config $config_file
-# echo "alpha = 0.10" >> $config_file
 $env_python $eval_file -ro 0 -tea $test_after -ld $log_dir -mid $m -ps $procs_per_machine -ms $machines -is $iterations -gf $graph -ta $test_after -cf $config_file -ll $log_level -wsd $log_dir
--- tutorial/run_federated.sh
+++ tutorial/run_federated.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-decpy_path=/mnt/nfs/risharma/Gitlab/decentralizepy/eval
+decpy_path=../eval # Path to eval folder
+graph=regular_16.txt # Absolute path of the graph file
+config_file=config.ini
+cp $graph $config_file $decpy_path
 cd $decpy_path
 
-env_python=~/miniconda3/envs/decpy/bin/python3
-graph=/mnt/nfs/risharma/Gitlab/tutorial/96_regular.edges
-original_config=/mnt/nfs/risharma/Gitlab/tutorial/config_celeba_sharing.ini
-config_file=~/tmp/config.ini
-procs_per_machine=16
-machines=1
+env_python=~/miniconda3/envs/decpy/bin/python3 # Path to python executable of the environment | conda recommended
+machines=1 # number of machines in the runtime
 iterations=80
 test_after=20
-eval_file=testingFederated.py
-log_level=INFO
+eval_file=testing.py # decentralized driver code (run on each machine)
+log_level=INFO # DEBUG | INFO | WARN | CRITICAL
+
 server_rank=-1
 server_machine=0
 working_rate=0.5
 
-m=`cat $(grep addresses_filepath $original_config | awk '{print $3}') | grep $(/sbin/ifconfig ens785 | grep 'inet ' | awk '{print $2}') | cut -d'"' -f2`
+m=0 # machine id corresponding consistent with ip.json
 echo M is $m
-log_dir=$(date '+%Y-%m-%dT%H:%M')/machine$m
+
+procs_per_machine=16 # 16 processes on 1 machine
+echo procs per machine is $procs_per_machine
+
+log_dir=$(date '+%Y-%m-%dT%H:%M')/machine$m # in the eval folder
 mkdir -p $log_dir
 
-cp $original_config $config_file
-# echo "alpha = 0.10" >> $config_file
 $env_python $eval_file -ro 0 -tea $test_after -ld $log_dir -mid $m -ps $procs_per_machine -ms $machines -is $iterations -gf $graph -ta $test_after -cf $config_file -ll $log_level -ctr 0 -cte 0 -wsd $log_dir -sm $server_machine -sr $server_rank -wr $working_rate
